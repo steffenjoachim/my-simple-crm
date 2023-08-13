b2a4cb212aab1b3f1ee1165d13717e454ec29bde
steffenjoachim <steffenjoachim.schanze@gmail.com>
2023-08-13 12:53:06 +0200

    after changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: AppComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'user', component: UserComponent},
   {path: 'user/:id', component: UserDetailComponent}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -9,14 +10,34 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  drawer: any;
   title(title: any) {
     throw new Error('Method not implemented.');
   }
   firestore: Firestore = inject(Firestore)
   items$: Observable<any[]>;
+  isSmallScreen = false;
 
   constructor() {
     const aCollection = collection(this.firestore, 'items')
     this.items$ = collectionData(aCollection);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  ngOnInit() {
+    this.checkScreenSize(); // Überprüfe die Bildschirmgröße beim Initialisieren der Komponente
+  }
+
+  private checkScreenSize() {
+    this.isSmallScreen = window.innerWidth <= 900;
+  }
+
+  toggleDrawer() {
+    // Setze das 'opened' Attribut basierend auf der Bildschirmgröße
+    this.drawer.opened = !this.isSmallScreen;
   }
 }
