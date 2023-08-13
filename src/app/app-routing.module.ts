7e1033166d28ee631b1015e5a66204656c1116b0
steffenjoachim <steffenjoachim.schanze@gmail.com>
2023-08-13 17:27:58 +0200

    after changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
+  {path: '', component: DashboardComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'user', component: UserComponent},
   {path: 'user/:id', component: UserDetailComponent}
